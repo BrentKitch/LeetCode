e03f44b7d47f820bd9ed3c40d33c5f6c2c9b897c
Brent Kitchen <brent.kitchen@outlook.com>
2020-01-29 09:43:54 -0500

    Added TwoSums solution

---
+++ TwoSums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Leet_Code
+{
+    class TwoSums
+    {
+        static void Main(string[] args)
+        {
+            int[] testarray = { 2, 8, 6, 1, 5, 15, 17, 19, 30, 11, 7, 4 };
+            Console.WriteLine(String.Join(" ", Sum(testarray,14)));
+        }
+        static int[] Sum(int[] nums, int target)
+        {
+           
+            for(int i = 0; i < nums.Length -1 ; i++)
+            {
+                    for (int b = i + 1; b < nums.Length ; b++)
+                    {
+                        if (nums[i] + nums[b] == target)
+                        { 
+                            int[] solution = { 1, b };
+                            return solution;
+                        }
+                    }
+            }
+            return null;
+        }
+    }
+}
